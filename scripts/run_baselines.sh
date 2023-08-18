1b5fae8ca5b8e6f900d138aed267800895b54a7c
Alok Kamatar <alokvk2@peony.a.cs.uchicago.edu>
2023-08-18 11:44:46 -0500

    Updating run script

--- scripts/run_baselines.sh
+++ scripts/run_baselines.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-layer=16
 features=1024
-for sparsity in "0.05" "0.1" "0.05" "0.01"; do
+for layer in "2" "4" "8" "120"; do
     for algo in "scipy" "pytorch" "ours"; do
         run_cmd="python SpDNN/python/runBaseline.py \
-                    -n dataset/networks/random/${sparsity} \
+                    -n dataset/networks/radix \
                     -a ${algo} \
                     -l ${layer} \
                     -f ${features} \
                     -i dataset/inputs/sparse-images-1024.tsv \
-                    -o baseline_results/inference_times.txt"
+                    -o baseline_results/inference_times.csv"
 
         perf stat -e L1-dcache-loads,L1-dcache-load-misses \
-                    -r 10 \
                     -x , \
                     -o baseline_results/temp.csv \
                     --append \
@@ -25,5 +23,8 @@ for sparsity in "0.05" "0.1" "0.05" "0.01"; do
         sed "s|\$|${exp}|" baseline_results/temp.csv >> baseline_results/cache_results.csv
         rm baseline_results/temp_2.csv
         rm baseline_results/temp.csv
+
+	echo .
     done
+    echo "Finished layer ${layer}"
 done
